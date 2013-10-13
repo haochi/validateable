d56608955b0c9861f3cc5dd26c2cdbc390707a6a
Haochi Chen <haochi.chen@gmail.com>
2013-10-13 11:10:37 -0700

    add isIn and matches

--- test/test.js
+++ test/test.js
@@ -2,22 +2,58 @@ var validateable = require('../lib/validateable');
 var assert = require("assert");
 
 describe('validateable#StringProperty', function(){
-  var string = new validateable.StringProperty;
+
   describe('constructor', function(){
     it('should be empty', function(){
+      var string = new validateable.StringProperty;
       assert.equal(string.get().length, 0);
       assert.equal(string.validate(), true);
     })
-    it("should pass: alpha", function(){
+  })
+
+  describe("isAlpha", function(){
+    it("should validate", function(){
+      var string = new validateable.StringProperty;
       string.set("abc");
       string.isAlpha();
       assert.equal(string.validate(), true);
     })
-    it("should fail: not alpha", function(){
+    it("should not validate", function(){
+      var string = new validateable.StringProperty;
       string.set("1");
       string.isAlpha();
       assert.equal(string.validate(), false);
     })
   })
+
+  describe("isIn", function(){
+    it("should validate because value is in list", function(){
+      var string = new validateable.StringProperty;
+      string.isIn({ list: ["1", "2"] });
+      string.set("1");
+      assert.equal(string.validate(), true);
+    })
+    it("should validate because value lowered case version is in list and validator is case insensity", function(){
+      var string = new validateable.StringProperty;
+      string.isIn({ list: ["a", "2"] });
+      string.set("A");
+      assert.equal(string.validate(), false);
+    })
+  });
+
+  describe("matches", function(){
+    it("should validate because it matches", function(){
+      var string = new validateable.StringProperty;
+      string.matches({ pattern: /hello/ });
+      string.set("hello");
+      assert.equal(string.validate(), true);
+    })
+    it("should not validate because it does not match", function(){
+      var string = new validateable.StringProperty;
+      string.matches({ pattern: /ho/ });
+      string.set("oh");
+      assert.equal(string.validate(), false);
+    })
+  });
 })
 
